33acf79ecc14a603c512f0cc1d79e720a2c0bd83
Josua Frank <frank.josua@gmail.com>
2022-03-05 18:03:31 +0100

    Fix for newly added two factor authentification

--- src/getLidlRefreshToken.js
+++ src/getLidlRefreshToken.js
@@ -3,25 +3,22 @@
  *
  * By: Bastiaan Steinmeier, https://github.com/basst85
  * Modified by: https://github.com/UNICodehORN
+ * Modified again by: https://github.com/Sharknoon
  *
  */
 
-
-/* Please fill in your details below */
-
-let login_email = 'YOUR@EMAIL';
-let login_password = 'YOUR LIDLPLUS PW';
-let login_country = 'NL';
-let login_language = 'NL-NL';
-
-/* NO CHANGES FROM HERE */
-
-const {Issuer, generators} = require('openid-client');
+const { Issuer, generators } = require('openid-client');
 const puppeteer = require('puppeteer');
 const devices = puppeteer.devices;
 const iPhone = devices['iPhone 5'];
 const urlparse = require('url');
 const req = require('request');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const util = require('util');
+const question = util.promisify(readline.question).bind(readline);
 
 Issuer.discover('https://accounts.lidl.com')
     .then(function (openidIssuer) {
@@ -40,24 +37,36 @@ Issuer.discover('https://accounts.lidl.com')
             code_challenge,
             code_challenge_method: 'S256'
         });
-        console.log('In case your refresh_token cannot be retrieved open this url once in your browser and accept the terms and conditions of the given country:\n');
-        console.log(loginurl + '&Country=' + login_country + '&language=' + login_language);
 
         (async () => {
+            const login_country = await question('Please enter your country code (DE, NL, ...): ');
+            const login_language = await question('Please enter your language code (DE-DE, NL-NL, ...): ');
+
+            console.log('In case your refresh_token cannot be retrieved open this url once in your browser and accept the terms and conditions of the given country:\n');
+            console.log(loginurl + '&Country=' + login_country + '&language=' + login_language);
+
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
             await page.emulate(iPhone);
             await page.goto(loginurl + '&Country=' + login_country + '&language=' + login_language);
             await new Promise(r => setTimeout(r, 1000));
-            await page.click('#button_welcome_login', {waitUntil: 'networkidle0'});
+            await page.click('#button_welcome_login', { waitUntil: 'networkidle0' });
             await new Promise(r => setTimeout(r, 3000));
-            await page.click('[name="EmailOrPhone"]', {waitUntil: 'networkidle0'});
-            await page.keyboard.type(login_email, {waitUntil: 'networkidle0'});
-            await page.click('#button_btn_submit_email', {waitUntil: 'networkidle0'});
+            await page.click('[name="EmailOrPhone"]', { waitUntil: 'networkidle0' });
+            const login_email = await question('Please enter your LIDL Plus e-mail: ');
+            await page.keyboard.type(login_email, { waitUntil: 'networkidle0' });
+            await page.click('#button_btn_submit_email', { waitUntil: 'networkidle0' });
             await new Promise(r => setTimeout(r, 3000));
-            await page.click('[name="Password"]', {waitUntil: 'networkidle0'});
-            await page.keyboard.type(login_password, {waitUntil: 'networkidle0'});
+            await page.click('[name="Password"]', { waitUntil: 'networkidle0' });
+            const login_password = await question('Please enter your LIDL Plus password: ')
+            await page.keyboard.type(login_password, { waitUntil: 'networkidle0' });
+            await page.click('#button_submit', { waitUntil: 'networkidle0' });
             await new Promise(r => setTimeout(r, 3000));
+            await page.click('button', { waitUntil: 'networkidle0' });
+            await new Promise(r => setTimeout(r, 3000));
+            await page.click('#field_VerificationCode', { waitUntil: 'networkidle0' });
+            const code = await question('Please check your e-mails and enter the received code here: ');
+            await page.keyboard.type(code, { waitUntil: 'networkidle0' });
 
             page.on('request', request => {
                 if (request.isNavigationRequest()) {
@@ -79,7 +88,7 @@ Issuer.discover('https://accounts.lidl.com')
                             code_verifier: code_verifier
                         };
 
-                        req.post({url: tokenurl, form: form, headers: headers, json: true}, function (e, r, body) {
+                        req.post({ url: tokenurl, form: form, headers: headers, json: true }, function (e, r, body) {
                             console.log('BODY:\n', body, '\n');
                             console.log('Access token:\n', body.access_token, '\n');
                             console.log('Refresh token:\n', body.refresh_token);
@@ -95,7 +104,7 @@ Issuer.discover('https://accounts.lidl.com')
             });
 
             console.log("submit...");
-            const response = await page.click('#button_submit', {waitUntil: 'networkidle0'});
+            await page.click('button', { waitUntil: 'networkidle0' });
 
             await new Promise(r => setTimeout(r, 15000));
             await browser.close();
